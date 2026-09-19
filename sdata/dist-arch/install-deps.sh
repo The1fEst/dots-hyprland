f42e4caa41decb3e2ac0dc41bff16884aa51708c
fEst <refer.nfs@gmail.com>
2026-09-19 21:25:41 +0500

    fix: let force mode update the system without asking

--- sdata/dist-arch/install-deps.sh
+++ sdata/dist-arch/install-deps.sh
@@ -53,7 +53,7 @@ v remove_deprecated_dependencies
 # Issue #363
 case $SKIP_SYSUPDATE in
   true) true;;
-  *) v sudo pacman -Syu;;
+  *) $ask && v sudo pacman -Syu || v sudo pacman -Syu --noconfirm;;
 esac
 
 # Use paru as AUR helper
